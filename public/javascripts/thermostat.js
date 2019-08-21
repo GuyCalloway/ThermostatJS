8189d891c76a7ae45c9a9e020d7600f792ab4422
Guy Calloway <[guyfcalloway@gmail.com]>
2019-08-21 16:59:50 +0100

    Added power saving mode Added resetting function Starting to work on energy usage category

--- public/javascripts/thermostat.js
+++ public/javascripts/thermostat.js
@@ -1,9 +1,41 @@
 
 function Thermostat () {
+    //TODO try to remove magic number
     this.temperature = 20
+    this.powersaving = true
 }
 
 Thermostat.prototype = {
-   up: function() { this.temperature ++ },
-   down: function() { this.temperature -- }
+    up: function() {
+
+        //TODO refactor later
+        if(this.powersaving == true){
+            if(this.temperature == 25){
+                return; 
+            }else{
+                this.temperature++;
+            }
+        }else{
+            if(this.temperature == 32){
+                return;
+            }else{
+                this.temperature++;
+            }
+        }
+
+    },
+
+    down: function() { 
+        if (this.temperature > 10) {
+            this.temperature --
+        } else {
+            return;
+        };
+    },
+
+    reset: function() {
+        this.temperature = 20;
+    }
+
+
 }
--- spec/javascripts/thermostatSpec.js
+++ spec/javascripts/thermostatSpec.js
@@ -1,28 +1,81 @@
 'use strict';
 
 describe('Thermostat', function() {
+  var thermostat;
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
+
+
   describe('#', function() {
-        it("has default temperature of 20 degrees", function() {
-        var thermostat = new Thermostat()
-        expect(thermostat.temperature).toBe(20)
-      });
+    it("has default temperature of 20 degrees", function() {
+      // var thermostat = new Thermostat()
+      expect(thermostat.temperature).toBe(20)
     });
+
+    it("has power-saving mode on by default", () => {
+        expect(thermostat.powersaving).toBe(true);
+    });  
+  });
 
     describe('#up', function() {
 
         it("the temperature can be increased incrementally", function() {
-          var thermostat = new Thermostat()
+          // var thermostat = new Thermostat()
           thermostat.up()
           expect(thermostat.temperature).toBe(21)
         });
+
+        it("can not exceed temp to 26 degree in power-saving mode", () => {
+          thermostat.temperature = 25;
+          thermostat.up();
+          expect(thermostat.temperature).toBe(25);
+        });
+
+        it("can not exceed 32 degrees in normal mode", () => {
+          thermostat.powersaving = false
+          thermostat.temperature = 32
+          thermostat.up();
+          expect(thermostat.temperature).toBe(32);
+        });
+
+
       });
 
       describe('#down', function() {
 
         it("the temperature can be increased incrementally", function() {
-          var thermostat = new Thermostat()
+          // var thermostat = new Thermostat()
           thermostat.down()
           expect(thermostat.temperature).toBe(19)
+        });
+
+        it("can not go below temp to 10 degree", () => {
+          thermostat.temperature = 10;
+          thermostat.down();
+          expect(thermostat.temperature).toBe(10);
+        });
+      });
+
+      describe("#reset", () => {
+        it("reset temperature to 20", () => {
+          thermostat.temperature = 15;
+          thermostat.reset();
+          expect(thermostat.temperature).toBe(20);
+        });
+      });
+
+      describe("#energyCheck", () => {
+        it("return correct energy usage category for current temperature", () => {
+          //low-usage
+          thermostat.temperature = 17;
+          expect(thermostat.usage()).toBe('low-usage');
+          //medium-usage
+          thermostat.temperature = 24;
+          expect(thermostat.usage()).toBe('medium-usage');
+          //high-usage
+          thermostat.temperature = 26;
+          expect(thermostat.usage()).toBe('high-usage');
         });
       });
 });
